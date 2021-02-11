f5ecdc9e11cce2a2cd9bb1348bc6eb6e297b4d6d
ourongxing <orongxing@gmail.com>
2021-02-11 16:17:21 +0800

    update 超时重试10次都能失败，看来是打卡的IP问题，只能设置成两个定时打卡。

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ ax.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
 });
 
 //默认超时时间
-ax.defaults.timeout = 15000;
+ax.defaults.timeout = 10000;
 //默认重试次数
 ax.defaults.retry = 3;
 //默认间隔时间
